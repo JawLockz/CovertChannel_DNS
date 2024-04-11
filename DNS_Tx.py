220a4bdc9d8e70893f7a39496aba905cbe0bb06e
Ashler Benda <ashlerb@iastate.edu>
2024-04-10 19:01:19 -0500

    Working POC for Transmit file

--- DNS_Tx.py
+++ DNS_Tx.py
@@ -3,33 +3,33 @@ import os
 import math
 
 DNS_Destination = {
-    "8.8.8.8":"0",
-    "8.8.4.4":"1",
-    "76.76.2.0":"2",
-    "76.76.10.0":"3",
-    "9.9.9.9":"4",
-    "149.112.112.112":"5",
-    "208.67.222.222":"6",
-    "208.67.220.220":"7",
-    "1.1.1.1":"8",
-    "1.0.0.1":"9",
-    "94.140.14.14":"10",
-    "94.140.15.15":"11",
-    "185.228.168.9":"12",
-    "185.228.169.9":"13",
-    "76.76.19.19":"14",
-    "76.223.122.150":"15"
+    "0": "8.8.8.8",
+    "1": "8.8.4.4",
+    "2": "76.76.2.0",
+    "3": "76.76.10.0",
+    "4": "9.9.9.9",
+    "5": "149.112.112.112",
+    "6": "208.67.222.222",
+    "7": "208.67.220.220",
+    "8": "1.1.1.1",
+    "9": "1.0.0.1",
+    "10": "94.140.14.14",
+    "11": "94.140.15.15",
+    "12": "185.228.168.9",
+    "13": "185.228.169.9",
+    "14": "76.76.19.19",
+    "15": "76.223.122.150"
 }
 
 DNS_qname = {
-    "youtube.com":"0",
-    "www.google.com":"1",
-    "www.blogger.com":"2",
-    "linkedin.com":"3",
-    "cloudflare.com":"4",
-    "microsoft.com":"5",
-    "apple.com":"6",
-    "support.google.com":"7"    
+    "0": "youtube.com",
+    "1": "www.google.com",
+    "2": "www.blogger.com",
+    "3": "linkedin.com",
+    "4": "cloudflare.com",
+    "5": "microsoft.com",
+    "6": "apple.com",
+    "7": "support.google.com"
 }
 
 def send_dns_query(domain_name, dns_server):
@@ -49,7 +49,7 @@ def send_dns_query(domain_name, dns_server):
 #Function to check if the given message is ascii to ensure it can be transmitted using this method
 def is_ascii(s):
     return all(ord(c) < 128 for c in s)
-
+  
 def toBinary(a):
   ascii_nums,binary=[],[]
   for i in a:
@@ -69,8 +69,11 @@ while not(ascii):
     if not (ascii):
         print("Given string contains non-ascii characters, try again.")
 
-
-
-domain_name = "netflix.com"  # Domain name to query
-dns_server = "8.8.8.8"  # DNS server IP address
-send_dns_query(domain_name, dns_server)
+binary = toBinary(message)
+print(binary)
+for char in binary:
+    domain_name = str(char)[:3]  # Domain name to query
+    dns_server = str(char)[3:7]  # DNS server IP address
+    domain_name = DNS_qname[str(int(domain_name, 2))]
+    dns_server = DNS_Destination[str(int(dns_server, 2))]
+    send_dns_query(domain_name, dns_server)
