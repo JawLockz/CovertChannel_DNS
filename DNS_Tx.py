fdec97c0e49beb4f9b045f981b5e3af7c29ed70b
Ashler Benda <ashlerb@iastate.edu>
2024-04-23 18:11:02 -0500

    replaced pcap

--- DNS_Tx.py
+++ DNS_Tx.py
@@ -38,8 +38,8 @@ def send_dns_query(domain_name, dns_server):
     # Create DNS query packet
     dns_query_packet = IP(dst=dns_server)/UDP(dport=53)/DNS(rd=1, qd=DNSQR(qname=domain_name, qtype='A'))
 
-    # Send DNS query packet and receive response
-    dns_response = sr1(dns_query_packet, verbose=0)
+    # Send DNS query packet and receive response with timeout
+    dns_response = sr1(dns_query_packet, verbose=0, timeout=5)
 
     # Process DNS response
     if dns_response and dns_response.haslayer(DNS):
